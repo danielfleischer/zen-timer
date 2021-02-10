87e8b2916b26f3b084d4194f944e0ec58f17c963
Daniel Fleischer <danflscr@gmail.com>
2021-02-10 22:31:02 +0200

    Refactor

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const figlet = require("figlet");
 const inquirer = require("inquirer");
 const clui = require("clui");
 const util = require("lodash/util");
-var player = require('play-sound')(opts = {})
+const player = require("play-sound")((opts = {}));
+const ProgressBar = require("progress");
 
+function format_time(seconds) {
+  var date = new Date(0);
+  date.setSeconds(seconds);
+  var timeString = date.toISOString().substr(11, 8);
+  return timeString;
+}
 
 clear();
 
@@ -22,67 +29,63 @@ function askTimer() {
     {
       name: "minutes",
       type: "list",
-      default: 29,
+      default: 0,
       choices: util.range(1, 121),
       message: "Select meditation length:",
     },
     {
       name: "bell",
       type: "list",
-      default: "middle",
-      choices: util.range(1, 21).concat("middle"),
+      default: "None",
+      choices: util.range(1, 31).concat("None", "Middle"),
       message: "Select interval timer:",
     },
   ];
   return inquirer.prompt(questions);
 }
 
-var ProgressBar = require("progress");
+function setBar(results, finish) {
+  return new ProgressBar(
+    `:my_time  :bar  Finish: ${finish.toTimeString().slice(0, 5)}`,
+    {
+      complete: "=",
+      head: ">",
+      incomplete: " ",
+      width: 60,
+      total: results.minutes * 60,
+    }
+  );
+}
 
-function format_time(seconds) {
-  var date = new Date(0);
-  date.setSeconds(seconds);
-  var timeString = date.toISOString().substr(11, 8);
-  return timeString;
-} 
+function setTimer(bar, finish) {
+  setInterval(function () {
+    bar.tick({
+      my_time: format_time(bar.curr),
+      finish: finish,
+    });
+    if (bar.complete) {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
 
-
-const run = async () => {
+async function run() {
   const results = await askTimer();
-  console.log();
 
-  
-  player.play('bell1.mp3', function(err){
-    if (err) throw err
-  })
+  console.log();
+  player.play("bell1.mp3", function (err) {
+    if (err) throw err;
+  });
 
   var now = new Date();
   var finish = new Date(now.setMinutes(now.getMinutes() + results.minutes));
 
-  var bar = new ProgressBar(`:my_time  :bar  Finish: ${finish.toTimeString().slice(0,5)}`, {
-    complete: "=",
-    head: ">",
-    incomplete: " ",
-    width: 60,
-    total: results.minutes * 60,
+  var bar = setBar(results, finish)
+  var timer = setTimer(bar, finish)
+
+  player.play("bell1.mp3", function (err) {
+    if (err) throw err;
   });
-
-  var timer = setInterval(function () {
-    bar.tick({ 
-      my_time: format_time(bar.curr),
-      finish: finish
-    });
-    if (bar.complete) {
-      clearInterval(timer);
-  
-      player.play('bell1.mp3', function(err){
-        if (err) throw err
-      })
-
-    }
-  }, 1000);
-
-  //   console.log(thisProgressBar.update(1,2));
-};
+}
 
 run();
